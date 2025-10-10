cfc79193a8d91623ffdcfaf8f6d91a7fa63920e3
серега24-41 <cool.dnevnik-ru2013@yandex.ru>
2025-10-10 18:03:11 +0300

    ,

--- lab1/match.php
+++ lab1/match.php
@@ -1,6 +1,5 @@
 <?php
-    $a = 6;
-    $day = (int) $a;
+$day = 3;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -8,20 +7,17 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Конструкция switch</title>
+	<title>Конструкция match</title>
 </head>
 <body>
-	<h1>Конструкция switch</h1>
+	<h1>Конструкция match</h1>
 	<?php
-
-    $output = match (true) {
-        $day < 1 || $day >7 => "Неизвестный день",
-        $day > 1 && $day <6 => "Это рабочий день",
-        $day > 5 && $day <8 => "Это выходной день",
-};
-
-var_dump($output);
-
+	$result = match(true) {
+	    $day >= 1 && $day <= 5 => "Это рабочий день",
+	    $day >= 6 && $day <= 7 => "Это выходной день",
+	    default => "Неизвестный день"
+	};
+	echo $result;
 	?> 
 </body>
 </html>
